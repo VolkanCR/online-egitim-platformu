067508af4ba8726e34d2264f567da8df6acb137b
VolkanCR <volkan.acar@hotmail.com>
2024-01-23 14:31:50 +0300

    Site temeli olusturuldu

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <div>
+
+      
+    <Button>
+      Click me
+    </Button>
+    
+
+    </div>
+
+    
+    
+  )
+}
