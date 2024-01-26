fcd1a6c2099ecfc4c9fa0ba387fc4bbbd344320c
VolkanCR <volkan.acar@hotmail.com>
2024-01-26 18:09:24 +0300

    duzenlemeler

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,19 +6,19 @@ async function main() {
   try {
     await database.category.createMany({
       data: [
-        { name: "Bilgisayar bilimleri" },
-        { name: "Müzik" },
-        { name: "Spor" },
-        { name: "Fotoğrafçılık" },
-        { name: "Muhasebe" },
-        { name: "Mühendislik" },
-        { name: "Film" },
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engineering" },
+        { name: "Filming" },
       ]
     });
 
-    console.log("Başarılı");
+    console.log("Success");
   } catch (error) {
-    console.log("Veritabanı kategorileri eklenirken hata oluştu", error);
+    console.log("Error seeding the database categories", error);
   } finally {
     await database.$disconnect();
   }
